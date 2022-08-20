d2471944685ca9b1bc913052795a87fa02e688a4
HenryH <henriquehaag11@gmail.com>
2022-08-20 18:32:26 -0300

    Terceira semana

---
+++ Terceira_semana/Lista_1/src/Formas.java
@@ -0,0 +1,13 @@
+public class Formas {
+    private Circulo[] circulos;
+
+    public Formas(int quant) {
+        circulos = new Circulo[quant];
+    }
+    public Circulo[] getCirculos() {
+        return circulos;
+    }
+    public void setCirculos(Circulo[] circulos) {
+        this.circulos = circulos;
+    }
+}
