69e892e9ce13e518f90e3a87084e1e319f857144
HenryH <henriquehaag11@gmail.com>
2022-08-20 18:30:03 -0300

    Terceira semana

---
+++ Terceira_semana/Lista_1/src/Circulo.java
@@ -0,0 +1,35 @@
+public class Circulo {
+    private double x, y, raio;
+
+    public Circulo(double x, double y, double raio){
+        this.x = x;
+        this.y = y;
+        this.raio = raio;
+    }
+    public double getX() {
+        return x;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public double getY() {
+        return y;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public double getRaio() {
+        return raio;
+    }
+    public void setRaio(double raio) {
+        this.raio = raio;
+    }
+
+    public double calculaCircunferencia(){
+        return Math.PI * raio * 2;
+    }
+
+    public double calculaArea(){
+        return Math.PI * Math.pow(raio, 2);
+    }
+}
