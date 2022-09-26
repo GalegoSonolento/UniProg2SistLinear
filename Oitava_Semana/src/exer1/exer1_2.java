77477f67876ececb1ba05585fc15d40c52dec227
Henrique Haag <henriquehaag11@gmail.com>
2022-09-26 18:04:29 -0300

    Commit

--- Oitava_Semana/src/exer1/exer1_2.java
+++ Oitava_Semana/src/exer1/exer1_2.java
@@ -3,11 +3,13 @@ package exer1;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class exer1_2 {
     public static void main(String[] args){
 
-        String path = "C:\\Users\\henri\\Documentos\\in.txt";
+        // String path = "C:\\Users\\henri\\Documentos\\in.txt";
+        String path = "C:\\Users\\Henrique Haag\\Documents\\in.txt";
 
         //Classe padrão para ler um streaming de dados
         FileReader fr = null;
@@ -20,9 +22,23 @@ public class exer1_2 {
             br = new BufferedReader(fr);
             // Uma leitura linha a linha é o jeito mais prático
 
+            String line = br.readLine();
+            while(line != null){
+                System.out.println(line);
+                line = br.readLine();
+            }
 
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        finally {
+            try{
+                br.close(); // O ato de fechar ambos esses recursos pode gerar uma exceção no código
+                fr.close();
+            }
+            catch (IOException e){
+                e.printStackTrace();    //Imprime na tela todo o stacktrace dos erros e ajuda a tratar na hora que der algum problema
+            }
         }
     }
 }
