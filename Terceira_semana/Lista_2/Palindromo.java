1731db218b77b4286486895c24cf493efe4f0c7f
Henrique Haag <henriquehaag11@gmail.com>
2022-09-28 17:38:14 -0300

    Commit

--- Terceira_semana/Lista_2/Palindromo.java
+++ Terceira_semana/Lista_2/Palindromo.java
@@ -1,9 +1,12 @@
 public class Palindromo {
     public static void main(String[] args){
-
+        palindromo("a bola da loba");
     }
 
-//    public boolean palindromo(String frase){
-//
-//    }
+    public static boolean palindromo(String frase){
+        String frase2 = frase.replace(" ", "");
+        System.out.println(frase2);
+        String[] letras = frase2.split("");
+        return true;
+    }
 }
