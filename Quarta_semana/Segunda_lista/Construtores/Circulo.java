46e0f146a40acab9c61df46871d77b10824a0014
HaagHenrique <henriquehaag11@gmail.com>
2022-10-07 22:11:40 -0300

    Commit

--- Quarta_semana/Segunda_lista/Construtores/Circulo.java
+++ Quarta_semana/Segunda_lista/Construtores/Circulo.java
@@ -1,17 +1,17 @@
 package Construtores;
 
-public class FormaCirculo {
+public class Circulo {
     private Ponto origem;
     private double raio;
 
-    public FormaCirculo(int raio){
+    public Circulo(int raio){
         this(new Ponto(0, 0), raio);
     }
-    public FormaCirculo(Ponto origem, double raio) {
+    public Circulo(Ponto origem, double raio) {
         this.origem = origem;
         this.raio = raio;
     }
-    public FormaCirculo(int raio, int x, int y){
+    public Circulo(int raio, int x, int y){
         this.origem = new Ponto(x, y);
         this.raio = raio;
     }
