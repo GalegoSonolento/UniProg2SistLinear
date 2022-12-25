5d369f39157b8aca52be40adc61713f0225ef4ab
henri <henriquehaag11@gmail.com>
2022-12-25 16:50:43 -0300

    Listas

--- Semana_9/src/ex06/Disciplina.java
+++ Semana_9/src/ex06/Disciplina.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class Disciplina {
     private ArrayList<Aluno> alunos;
+    private double maiorMedia = 0;
+    private double menorMedia = 11, maiorNota = 0, menorNota = 11;
 
     public Disciplina() {
         alunos = new ArrayList<>();
@@ -18,10 +20,34 @@ public class Disciplina {
     }
 
     public void exibeNotas(){
-        String input = "Estudante: ";
         System.out.printf("%s\t %s\t %s\t %s\n", "Estudante: ", "A", "B", "Média");
         for (Aluno al : alunos){
-            System.out.println();
+            double media = (al.getNotaA() + al.getNotaB())/2;
+            if (media > maiorMedia) maiorMedia = media;
+            if (media < menorMedia) menorMedia = media;
+            System.out.printf("%s\t %.2f\t %.2f\t %.2f\n", al.getNome(), al.getNotaA(), al.getNotaB(), media);
         }
+    }
+
+    public void exibeMedias(){
+        for (Aluno al : alunos){
+            if (al.getNotaA() > maiorNota) maiorNota = al.getNotaA();
+            if (al.getNotaA() < menorNota) menorNota = al.getNotaA();
+            if (al.getNotaB() > maiorNota) maiorNota = al.getNotaB();
+            if (al.getNotaB() < menorNota) maiorNota = al.getNotaB();
+        }
+        System.out.println("MÉDIAS: ");
+        double somaGeral = 0;
+        for (Aluno al : alunos){
+            somaGeral += al.getNotaA();
+            somaGeral += al.getNotaB();
+        }
+        int qtdeNotas = alunos.size() * 2;
+        double mediaGeral = somaGeral / qtdeNotas;
+        System.out.printf("Média geral: %.2f%n", mediaGeral);
+        System.out.printf("Maior média: %.2f%n", maiorMedia);
+        System.out.printf("Menor média: %.2f%n", menorMedia);
+        System.out.printf("Menor nota: %.2f%n", menorNota);
+        System.out.printf("Maior nota: %.2f%n", maiorNota);
     }
 }
--- Semana_9/src/ex06/Main.java
+++ Semana_9/src/ex06/Main.java
@@ -1,10 +1,38 @@
+//Faça um programa que gerencie as notas de 10 alunos. A classe Aluno deve conter o nome do aluno e a
+//        notas de grau A, grau B. Crie uma classe Disciplina que manipulará um ArrayList de alunos. O programa
+//        deve exibir o nome e a nota dos alunos, bem como sua média (usem média aritmética para calculá-la). Ao
+//final, além da exibição, programa deve emitir a média geral da turma, a menor nota, a maior nota, a menor
+//        média e a maior média.
+//        Exemplo:
+//        Estudante A B Média
+//        Guilherme 8 10 9
+//        Joao 10 6 8
+//        ....
+//        MEDIAS
+//        Media geral: 8.5
+//        Menor media: 5.4
+//        Maior media: 9
+//        Menor nota: 3
+//        Maior nota: 10
+
 package ex06;
 
 public class Main {
     public static void main(String[] args){
         Disciplina ds = new Disciplina();
 
+        Aluno al1 = new Aluno("Jorge", 10, 5);
+        Aluno al2 = new Aluno("Jorgina", 8, 9);
+        Aluno al3 = new Aluno("samuel", 5.5, 7);
+        Aluno al4 = new Aluno("Petrolina", 8.2, 2);
+
+        ds.getAlunos().add(al1);
+        ds.getAlunos().add(al2);
+        ds.getAlunos().add(al3);
+        ds.getAlunos().add(al4);
+
         ds.exibeNotas();
+        ds.exibeMedias();
 
 
 
